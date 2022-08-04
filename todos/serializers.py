5daeb6a3759c67b6efce33f528d968e20500d2c3
Veselin Lazarevic <veselin.lazarevic@omninext.it>
2022-08-04 12:25:13 +0200

    aaaaaa

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from todos.models import Todo, Movie, Participant, Title, Slike, PrikazSlika
-#Programmer, Language, Paradigm, Tag, TagImage
 
 
 class TodoSerializer(ModelSerializer):
@@ -9,30 +8,6 @@ class TodoSerializer(ModelSerializer):
     class Meta:
         model=Todo
         fields = ('id','title', 'description', 'is_complete',)
-
-
-# class TagImageSerializer(serializers.ModelSerializer):
-    
-#     class Meta:
-#         model = TagImage
-#         fields = '__all__'
-
-# class TagSerializer(serializers.HyperlinkedModelSerializer):
-    
-#      class Meta:
-#         model = Tag
-#         fields = '__all__'
-
-
-
-        #read_only_field = ['id']
-    
-    #  def create(self, validated_data):
-    #         images_data = validated_data.pop('images')
-    #         tag = Tag.objects.create(**validated_data)
-    #         for image_data in images_data:
-    #             TagImage.objects.create(tag=tag, **image_data)
-    #         return tag
 
 
 
@@ -52,25 +27,7 @@ class ParticipantSerializer(serializers.HyperlinkedModelSerializer):
         read_only_field = ['id']
 
 
-# class LanguageSerializer(serializers.HyperlinkedModelSerializer):
 
-#     class Meta:
-#         model = Language
-#         fields = ('id', 'url', 'name', 'paradigm')
-
-
-
-# class ParadigmSerializer(serializers.HyperlinkedModelSerializer):
-
-#     class Meta:
-#         model = Paradigm
-#         fields = ('id', 'name')
-
-# class ProgrammerSerializer(serializers.HyperlinkedModelSerializer):
-    
-#     class Meta:
-#         model = Programmer
-#         fields = ('id','name', 'language')
 
 
 class MovieSerializer(serializers.HyperlinkedModelSerializer):
--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from todos.models import Todo, Movie, Participant, Title, Slike, PrikazSlika
-#Language, Programmer, Paradigm
-from rest_framework import permissions, filters, viewsets, mixins
+
+from rest_framework import permissions, filters, viewsets
 from todos.serializers import TodoSerializer, MovieSerializer, ParticipantSerializer, TitleSerializer, PrikazSlikaSerializer, SlikeSerializer
-#LanguageSerializer, ProgrammerSerializer, ParadigmSerializer
+
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -26,24 +26,6 @@ class TodosAPIView(ListCreateAPIView):
     
     def get_queryset(self):
         return Todo.objects.filter(owner=self.request.user)
-
-# """Create API View automatski handluje post req"""
-# class CreateTodoApiView(CreateAPIView):
-#     serializer_class = TodoSerializer
-#     permission_classes = (IsAuthenticated,)
-
-
-#     def perform_create(self, serializer):
-#         return serializer.save(owner=self.request.user)
-
-
-# class TodoListAPIView(ListAPIView):
-#     serializer_class = TodoSerializer
-#     permission_classes = (IsAuthenticated,)
-
-
-#     def get_queryset(self):
-#         return Todo.objects.filter(owner=self.request.user)
 
 
 class TodoDetailAPIView(RetrieveUpdateDestroyAPIView):
@@ -67,11 +49,6 @@ class MovieViewSet(viewsets.ModelViewSet):
     filterset_fields = ['title', 'description','participants']
 
 
-# class TagViewSet(viewsets.ModelViewSet):
-#     """Maange tags in the datbase"""
-#     queryset = Tag.objects.all()
-#     serializer_class = TagSerializer
-
 
 class ParticipantViewSet(viewsets.ModelViewSet):
     """Maange tags in the datbase"""
@@ -84,21 +61,6 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
 
-
-
-# class LanguageView(viewsets.ModelViewSet):
-#     queryset = Language.objects.all()
-#     serializer_class = LanguageSerializer
-
-
-# class ParadigmView(viewsets.ModelViewSet):
-#     queryset = Paradigm.objects.all()
-#     serializer_class = ParadigmSerializer
-
-
-# class ProgrammerView(viewsets.ModelViewSet):
-#     queryset = Programmer.objects.all()
-#     serializer_class = ProgrammerSerializer
 
 
 class SlikeView(viewsets.ModelViewSet):
